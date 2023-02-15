a0280f3e44fe221cde0631a5babebbe19b20f851
Eric Larson <ewlarson@gmail.com>
2023-02-15 16:52:09 -0600

    Update development.rb
    Add NGROK to hosts

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # NGROK - ngrok http -subdomain=uwm-demo 3000
+  # config.hosts << "uwm-demo.ngrok.io"
 end
