aa9029665bfa9707972156083250d28ce944af22
Eric Larson <ewlarson@gmail.com>
2023-03-08 16:19:15 -0600

    Update catalog_controller_test.rb

--- test/controllers/catalog_controller_test.rb
+++ test/controllers/catalog_controller_test.rb
@@ -10,9 +10,4 @@ class CatalogControllerTest < ActionDispatch::IntegrationTest
     get "/catalog/mit-001145244/admin"
     assert_response :success
   end
-
-  test "should return admin view" do
-    get "/catalog/mit-001145244/admin"
-    assert_response :success
-  end
 end
